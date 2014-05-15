a3e15177425e1103e0cc3b03ddec751882a8469f
Marcus Vinicius Balbi <marcusbalbi@hotmail.com>
2014-05-15 17:32:50 -0300

    acerto comentario classe

--- NFeFacade.php
+++ NFeFacade.php
@@ -1,4 +1,41 @@
 <?php
+/**
+ * Este arquivo é parte do projeto NFePHP - Nota Fiscal eletrônica em PHP.
+ *
+ * Este programa é um software livre: você pode redistribuir e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU (GPL)como é publicada pela Fundação
+ * para o Software Livre, na versão 3 da licença, ou qualquer versão posterior
+ * e/ou
+ * sob os termos da Licença Pública Geral Menor GNU (LGPL) como é publicada pela Fundação
+ * para o Software Livre, na versão 3 da licença, ou qualquer versão posterior.
+ *
+ * Este programa é distribuído na esperança que será útil, mas SEM NENHUMA
+ * GARANTIA; nem mesmo a garantia explícita definida por qualquer VALOR COMERCIAL
+ * ou de ADEQUAÇÃO PARA UM PROPÓSITO EM PARTICULAR,
+ * veja a Licença Pública Geral GNU para mais detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Publica GNU e da
+ * Licença Pública Geral Menor GNU (LGPL) junto com este programa.
+ * Caso contrário consulte <http://www.fsfla.org/svnwiki/trad/GPLv3> ou
+ * <http://www.fsfla.org/svnwiki/trad/LGPLv3>.
+ *
+ * Está atualizada para :
+ *      PHP 5.3
+ *
+ * Esta é a classe principal para a geração, controle e comunicação dos
+ * Conhecimentos de Transporte Eletrônicos CTe
+ *
+ * @package   NFePHP2
+ * @name      NFeFacade
+ * @version   1.0.19
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL v.3
+ * @copyright 2009-2012 &copy; CTePHP
+ * @link      http://www.nfephp.org/
+ * @author    Marcus V. Balbi <marcusbalbi@hotmail.com>
+ *
+ *
+ *
+ */
 
 namespace NFEPHP2;
 
@@ -14,6 +51,7 @@ use NFEPHP2\NFe\XML\Exceptions\NfeCancelarException;
 use NFEPHP2\NFe\XML\Exceptions\NfeProcException;
 use NFEPHP2\NFe\XML\Exceptions\NfeEnviarCCeException;
 use DOMDocument;
+
 /**
  * Description of NFe
  *
